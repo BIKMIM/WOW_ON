65219dff397bf3b4d16861731ebd7c23f6cb9ace
BIKMIM <2cp4us@gmail.com>
2021-12-19 15:38:59 +0900

    wow keep connect

---
+++ WOW_ON/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace WOW_ON
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.lblOnOff = new System.Windows.Forms.Label();
+            this.end_btn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(12, 60);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(75, 23);
+            this.start_btn.TabIndex = 0;
+            this.start_btn.Text = "시작";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // lblOnOff
+            // 
+            this.lblOnOff.AutoSize = true;
+            this.lblOnOff.Font = new System.Drawing.Font("굴림", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblOnOff.Location = new System.Drawing.Point(81, 9);
+            this.lblOnOff.Name = "lblOnOff";
+            this.lblOnOff.Size = new System.Drawing.Size(89, 37);
+            this.lblOnOff.TabIndex = 1;
+            this.lblOnOff.Text = "OFF";
+            this.lblOnOff.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // end_btn
+            // 
+            this.end_btn.Location = new System.Drawing.Point(168, 60);
+            this.end_btn.Name = "end_btn";
+            this.end_btn.Size = new System.Drawing.Size(75, 23);
+            this.end_btn.TabIndex = 5;
+            this.end_btn.Text = "종료";
+            this.end_btn.UseVisualStyleBackColor = true;
+            this.end_btn.Click += new System.EventHandler(this.end_btn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 95);
+            this.Controls.Add(this.end_btn);
+            this.Controls.Add(this.lblOnOff);
+            this.Controls.Add(this.start_btn);
+            this.Name = "Form1";
+            this.Text = "접속유지 by 빕";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Label lblOnOff;
+        private System.Windows.Forms.Button end_btn;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WOW_ON/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Timers;
+using System.Runtime.InteropServices;
+using Timer = System.Windows.Forms.Timer;
+
+namespace WOW_ON
+{
+
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")] private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")] private static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll")] private static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+
+        private const int SW_WINDOW_NORMAL = 1;
+        private const int SW_WINDOW_MINIMIZED = 2;
+        private const int SW_WINDOW_MAXIMIZED = 3;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Interval = 1;
+
+            timer1.Tick += new EventHandler(timer1_Tick);
+        }
+        
+        private static DateTime Delay(int MS)
+        {
+            DateTime ThisMoment = DateTime.Now;
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, MS);
+            DateTime AfterWards = ThisMoment.Add(duration);
+
+            while (AfterWards >= ThisMoment)
+            {
+                System.Windows.Forms.Application.DoEvents();
+                ThisMoment = DateTime.Now;
+            }
+            return DateTime.Now;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(uint vk, uint scan, uint flags, uint extraInfo);
+
+        [DllImport("user32.dll")]
+        private static extern uint MapVirtualKey(int wCode, int wMapType);
+
+        
+
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            work();
+        }
+
+        public void work()
+        {
+            Random rand = new Random();
+            int D_time = rand.Next(1, 300);
+
+            timer1.Enabled = true;                      // 타이머 온
+            timer1.Interval = rand.Next(300000, 600000);   // 타이머 5분(300초,300,000 MS)~10분(600초, 600,000 MS) 사이 랜덤 셋팅
+            timer1.Start();                             // 타이머 시작
+            
+            string W_name = "월드 오브 워크래프트";
+            IntPtr hWnd = FindWindow(null, W_name); // 윈도우 타이틀명으로 핸들을 찾음. 
+            if (!hWnd.Equals(IntPtr.Zero))
+            {
+                ShowWindowAsync(hWnd, SW_WINDOW_NORMAL); // 윈도우가 최소화 되어 있다면 활성화 
+                SetForegroundWindow(hWnd);  // 윈도우에 포커스를 줘서 최상위로 만든다
+                lblOnOff.Text = "ON";
+
+                keybd_event((byte)Keys.Space, 0, 0x00, 0); // 스페이스 바 누름
+                Delay(D_time);                              // 스페이스바 누르는 시간
+                keybd_event((byte)Keys.Space, 0, 0x02, 0); // 스페이스바 뗌
+            }
+        }
+
+        private void end_btn_Click(object sender, EventArgs e)
+        {
+            lblOnOff.Text = "OFF";  // 동작 종료 확인 텍스트
+            timer1.Stop();          //타이머 종료
+            timer1.Enabled = false; // 타이머 정지
+        }
+
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Random rand = new Random();
+            //int D_time = rand.Next(2000, 4000);
+            //Delay(D_time);
+            work();
+        }
+
+        
+    }
+}
+
